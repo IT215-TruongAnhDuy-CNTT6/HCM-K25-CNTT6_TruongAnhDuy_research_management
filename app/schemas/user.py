bea93985fa89a717ba833fe2182439ea99ddd4e6
truongduy2357 <truongduy2357@gmail.com>
2026-08-20 20:59:41 +0700

    Task ngay 1 - sua lan 1

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -26,3 +26,12 @@ class UserResponse(UserBase):
 
     class Config:
         from_attributes = True
+
+class TokenResponse(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+
+class RefreshRequest(BaseModel):
+    refresh_token: str
